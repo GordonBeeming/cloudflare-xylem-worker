2541bc3d50c785dfda50a227659f37620a9d9747
Gordon Beeming <me@gordonbeeming.com>
2025-06-17 00:24:56 +1000

    feat(security): add X-DNS-Prefetch-Control header and disable CSP for preview (#9)
    Add the "X-DNS-Prefetch-Control" header set to "on" to improve DNS
    prefetching behavior and potentially enhance page load performance.
    
    Comment out the Content-Security-Policy (CSP) for the preview domain
    "preview.gordonbeeming.com" to disable the restrictive policy temporarily,
    likely to facilitate debugging or deployment testing.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const securityHeaders = {
     "X-Frame-Options": "DENY",
     "X-Content-Type-Options": "nosniff",
     "Referrer-Policy": "strict-origin-when-cross-origin",
+    "X-DNS-Prefetch-Control": "on",
 };
 
 const sanitiseHeaders = {
@@ -57,9 +58,9 @@ export default {
         }
 
         let csp = "";
-        if (domain === "preview.gordonbeeming.com") {
-            csp = "default-src 'self'; script-src 'self' 'unsafe-inline' static.cloudflareinsights.com giscus.app cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net; img-src 'self' data:; font-src 'self' cdn.jsdelivr.net; object-src 'none'; frame-src giscus.app; worker-src 'self' blob:; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
-        }
+        // if (domain === "preview.gordonbeeming.com") {
+        //     csp = "default-src 'self'; script-src 'self' 'unsafe-inline' static.cloudflareinsights.com giscus.app cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' cdn.jsdelivr.net; img-src 'self' data:; font-src 'self' cdn.jsdelivr.net; object-src 'none'; frame-src giscus.app; worker-src 'self' blob:; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
+        // }
         if (csp.length > 0) {
             newHeaders.set("Content-Security-Policy", csp);
             newHeaders.set("X-Content-Security-Policy", csp);
